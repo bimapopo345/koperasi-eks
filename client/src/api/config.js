a5b66fd6e0f08fc4be1635ffaf073fe593976a13
bimapopo345 <bimapopo345@gmail.com>
2026-04-27 16:29:01 +0700

    Fix frontend deploy V2

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,1 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API_URL = String(
+  import.meta.env.VITE_API_URL ||
+    import.meta.env.VITE_SERVER_URL ||
+    "http://localhost:5000",
+).replace(/\/$/, "");
--- client/src/conf/conf.js
+++ client/src/conf/conf.js
@@ -1,5 +1,9 @@
 const conf = {
-  server_url: String(import.meta.env.VITE_SERVER_URL).replace(/\/$/, ""),
+  server_url: String(
+    import.meta.env.VITE_SERVER_URL ||
+      import.meta.env.VITE_API_URL ||
+      "http://localhost:5000",
+  ).replace(/\/$/, ""),
 };
 
 export default conf;
